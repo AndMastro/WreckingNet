4270559e709c330937530608497f734dc2b78b19
AleMac <alessandro.maccagno@hotmail.com>
2019-02-09 23:44:59 +0100

    Your orders have removed 47 audio files from the gitignore. Someone's got to be blamed for your edit, Umberto. Who's it going to be?

--- src/audio_splitter.py
+++ src/audio_splitter.py
@@ -8,15 +8,17 @@ Created on Thu Feb  7 20:41:42 2019
 from pydub import AudioSegment
 import os
 
-DATAPATH = "../dataset/UtahAudioData"
-OUT = "../dataset/segments"
-AUDIOMS = 30000
+# =============================================================================
+# DATAPATH = "../dataset/UtahAudioData"
+# OUT = "../dataset/segments"
+# AUDIOMS = 30000
+# =============================================================================
 
 def partition_dataset(datapath, out, ms):
     
     cats = os.listdir(datapath)
     for cat in cats:
-        dirpath = os.path.join(DATAPATH, str(cat))
+        dirpath = os.path.join(datapath, str(cat))
         for track in os.listdir(dirpath):
             trackpath = os.path.join(dirpath, str(track))
             audio = AudioSegment.from_file(trackpath)
@@ -34,5 +36,7 @@ def partition_dataset(datapath, out, ms):
                 segment.export(os.path.join(outpath, str(str(idx) + "." + form)), format=form)
 
 
-if __name__ == "__main__":
-    partition_dataset(DATAPATH, OUT, AUDIOMS)
+# =============================================================================
+# if __name__ == "__main__":
+#     partition_dataset(DATAPATH, OUT, AUDIOMS)
+# =============================================================================
