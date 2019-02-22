c4b2c7b6f7bb01efbc7f5e4825e2aae4a07f887c
AndMastro <andrea.mastropietro@gmail.com>
2019-02-22 16:13:53 +0100

    added results for 5000ms

--- src/PickleGenerator.py
+++ src/PickleGenerator.py
@@ -11,8 +11,8 @@ from pydub import AudioSegment
 
 dataPath = "../dataset/5Classes"
 percSplit = 0.7
-audioMS = 3000
-audioHop = 1500
+audioMS = 7000
+audioHop = 3500
 
 
 def partition_track(track_path, out_path, ms, hop=None, get_drop=False):
