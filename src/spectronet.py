5e1a43ecdd9081a9b8f4196d6f4c3437b3237746
Umberto Mazziotta <umberto961@live.it>
2019-03-29 10:02:04 +0100

    Changed size of filters

--- src/spectronet.py
+++ src/spectronet.py
@@ -6,31 +6,36 @@ class SpectroCNN(tf.keras.Model):
         super(SpectroCNN, self).__init__()
 
         self.conv1 = tf.layers.Conv2D(filters=24,
-                                      kernel_size=[6, 6],
+                                      kernel_size=[6, 2],
+                                      #kernel_size=[6, 6],
                                       strides=(1, 1),
                                       kernel_initializer=tf.initializers.glorot_uniform,
                                       padding="same",
                                       activation=tf.nn.relu)
         self.conv2 = tf.layers.Conv2D(filters=24,
-                                      kernel_size=[6, 6],
+                                      kernel_size=[6, 2],
+                                      #kernel_size=[6, 6],
                                       strides=(1, 1),
                                       kernel_initializer=tf.initializers.glorot_uniform,
                                       padding="same",
                                       activation=tf.nn.relu)
         self.conv3 = tf.layers.Conv2D(filters=48,
-                                      kernel_size=[5, 5],
+                                      kernel_size=[5, 1],
+                                      #kernel_size=[5, 5],
                                       strides=(2, 2),
                                       kernel_initializer=tf.initializers.glorot_uniform,
                                       padding="same",
                                       activation=tf.nn.relu)
         self.conv4 = tf.layers.Conv2D(filters=48,
-                                      kernel_size=[5, 5],
+                                      kernel_size=[5, 1],
+                                      #kernel_size=[5, 5],
                                       strides=(2, 2),
                                       kernel_initializer=tf.initializers.glorot_uniform,
                                       padding="same",
                                       activation=tf.nn.relu)
         self.conv5 = tf.layers.Conv2D(filters=64,
-                                      kernel_size=[4, 4],
+                                      kernel_size=[4, 1],
+                                      #kernel_size=[4, 4],
                                       strides=(2, 2),
                                       kernel_initializer=tf.initializers.glorot_uniform,
                                       padding="same",
@@ -44,22 +49,22 @@ class SpectroCNN(tf.keras.Model):
 
     def call(self, x, training=False):
         batch_size = x.shape[0]
-        print("Spectronet - input", x.shape)
+        #print("Spectronet - input", x.shape)
         x = self.conv1(x)
-        print("Spectronet - conv1", x.shape)
+        #print("Spectronet - conv1", x.shape)
         x = self.conv2(x)
-        print("Spectronet - conv2", x.shape)
+        #print("Spectronet - conv2", x.shape)
         x = self.conv3(x)
-        print("Spectronet - conv3", x.shape)
+        #print("Spectronet - conv3", x.shape)
         x = self.conv4(x)
-        print("Spectronet - conv4", x.shape)
+        #print("Spectronet - conv4", x.shape)
         x = self.conv5(x)
-        print("Spectronet - conv5", x.shape)
+        #print("Spectronet - conv5", x.shape)
         x = self.dense(tf.reshape(x, [batch_size, -1]))  # this is not so correct
-        print("Spectronet - dense", x.shape)
+        #print("Spectronet - dense", x.shape)
         #x = self.dense(tf.reshape(x, [-1, 49152]))
         x = self.dropout(x, training=training)
-        print("Spectronet - dropour", x.shape)
+        #print("Spectronet - dropout", x.shape)
 
         return self.logits(x)
 
